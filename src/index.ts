c3bd42dd6169268c122f085c3833bc6a6b02b706
Andres de Lago Gomez <delagoandres@gmail.com>
2018-02-14 12:59:45 -0600

    fixes a problem with package name

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 
-AppRegistry.registerComponent('irrational-engine', () => App);
+AppRegistry.registerComponent('irrationalEngine', () => App);
