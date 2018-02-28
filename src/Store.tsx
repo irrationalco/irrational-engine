527608f4cbc61ba741dca8404da60382001f2339
Andres de Lago Gomez <delagoandres@gmail.com>
2018-02-28 11:43:02 -0600

    Fixes compilation

--- src/Store.tsx
+++ src/Store.tsx
@@ -76,17 +76,17 @@ export async function tryLogin(username: string, password: string): Promise<bool
 
 export async function logOut() {
     throw Error('Not implemented');
-    try {
-        const res = await fetch(`${Config.API_URL}/sessions`, {
-            headers: {
-                // TODO: add the correct header and value to make an authenticated call
-            },
-            method: 'DELETE',
-        });
-        await AsyncStorage.removeItem('user');
-    } catch (e) {
-        throw Error("Couldn't log out");
-    }
+    // try {
+    //     const res = await fetch(`${Config.API_URL}/sessions`, {
+    //         headers: {
+    //             // TODO: add the correct header and value to make an authenticated call
+    //         },
+    //         method: 'DELETE',
+    //     });
+    //     await AsyncStorage.removeItem('user');
+    // } catch (e) {
+    //     throw Error("Couldn't log out");
+    // }
 }
 
 export async function isLoggedIn() {
