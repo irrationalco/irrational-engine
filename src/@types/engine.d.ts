4dd3a4b96cb7dbb9ef3d584be7f9c2c4446cfe2a
Andres de Lago Gomez <delagoandres@gmail.com>
2018-03-14 17:05:27 -0600

    Adds new funcitons to handle firebase backend

--- src/@types/engine.d.ts
+++ src/@types/engine.d.ts
@@ -1,6 +1,81 @@
 declare namespace engine {
     interface User {
         uid: string;
-        organization_id: number;
+        organizationId: number;
+    }
+
+    interface SurveyListing {
+        id: number;
+        lastUpdate: Date;
+        numberOfQuestions: number;
+        sizeEstimate: number;
+        name: string;
+        // logo: image;
+    }
+
+    interface Survey {
+        id: number;
+        //logo: image;
+        lastUpdate: Date;
+        questions: Question[];
+        randomized: boolean;
+        questionsPerRun?: number;
+        necesaryQuestions?: Question[];
+    }
+
+    interface Question {
+        id: number;
+        type: string; //for now
+        variations: Variation[];
+    }
+
+    interface Variation {
+        title: string;
+        image: string; //for now
+        imageDescription: string;
+    }
+
+    interface Open extends Variation {
+        min: number;
+        max: number;
+    }
+
+    interface Single_select extends Variation {
+        options: Option[];
+    }
+
+    interface Option {
+        id: number;
+        text: string;
+        image: string; //for now
+        imageDescription: string;
+    }
+
+    interface Multiple_select extends Single_select {
+
+    }
+
+    interface Slider extends Variation {
+        min: number;
+        max: number;
+        start: number;
+        step: number;
+        // decorators: decorator[];
+    }
+
+    interface Stars extends Variation {
+
+    }
+
+    interface Like extends Variation {
+
+    }
+
+    interface Comparative_sliders extends Slider {
+        slides: Slide[];
+    }
+
+    interface Slide extends Option {
+
     }
 }
